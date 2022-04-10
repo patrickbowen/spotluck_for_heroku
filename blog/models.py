22b052e7f9c2232f76fe55d65ac476422116384d
Vinay Samalla <vinaysamalla@Vinays-MacBook-Air.local>
2022-04-10 20:48:58 -0300

    message

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    description = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
-
-class Items(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    unit_price = models.DecimalField(max_digits=5, decimal_places=2)
-    chef = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
